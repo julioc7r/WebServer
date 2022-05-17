adf1c008e47e9dd5fc9188961e13f821c371e695
julioc7r <julioc7r@gmail.com>
2022-05-17 17:18:28 -0300

    Second Commit(Part B implementation)

--- WebServer.java
+++ WebServer.java
@@ -1,11 +1,15 @@
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.StringTokenizer;
 
-/* Tarefa de Programacão 1 - Julio Cesar Rogacheski */
+/* Tarefa de Programacão 1 - Criando um servidor Web Muti-Thread em Java */
 
 public final class WebServer
 {
@@ -15,21 +19,22 @@ public final class WebServer
         int port = 6789;
         // Establish the listen socket.
         ServerSocket server = new ServerSocket(port);
-        System.out.println("Servidor iniciado na porta 6789");
+            System.out.println("Servidor iniciado na porta 6789");
 
-        // Process HTTP service requests in an infinite loop.
-        while (true) {
-        // Listen for a TCP connection request.            
-        Socket cliente = server.accept();
+            // Process HTTP service requests in an infinite loop.
+            while (true) {
+            // Listen for a TCP connection request.            
+            Socket cliente = server.accept();
 
-        // Construct an object to process the HTTP request message.
-            HttpRequest request = new HttpRequest(cliente);
-            // Create a new thread to process the request.
-            Thread thread = new Thread(request);
-            // Start the thread.
-            thread.start();    
-            //server.close();
-        }
+            // Construct an object to process the HTTP request message.
+                HttpRequest request = new HttpRequest(cliente);
+                // Create a new thread to process the request.
+                Thread thread = new Thread(request);
+                // Start the thread.
+                thread.start();    
+                //server.close();
+            }
+        
     }
 }
 
@@ -71,17 +76,23 @@ final class HttpRequest implements Runnable
         while ((headerLine = br.readLine()).length() != 0) {
             System.out.println(headerLine);
         }
-        /*
+        
         // Extract the filename from the request line.
         StringTokenizer tokens = new StringTokenizer(requestLine);
         tokens.nextToken(); // skip over the method, which should be "GET"
         String fileName = tokens.nextToken();
         // Prepend a "." so that file request is within the current directory.
         fileName = "." + fileName;
-        
-        // Open the requested file.
+        System.out.println("->"+fileName);
+
+        if(fileName.compareTo("./") == 0){
+            fileName = "./foto.jpg";//"./index.html";
+        }
+        System.out.println("->"+fileName);// Open the requested file.
         FileInputStream fis = null;
         boolean fileExists = true;
+
+
         try {
         fis = new FileInputStream(fileName);
         } catch (FileNotFoundException e) {
@@ -91,36 +102,64 @@ final class HttpRequest implements Runnable
         String statusLine = null;
         String contentTypeLine = null;
         String entityBody = null;
+    
         if (fileExists) {
 
-        statusLine = ?;
+        statusLine ="HTTP/1.0 200 OK" + CRLF;
         contentTypeLine = "Content-type: " +
-        contentType( fileName ) + CRLF;
-
+        contentType(fileName) + CRLF;
         } else {
-        statusLine = ?;
-        contentTypeLine = ?;
+        statusLine = "HTTP/1.0 40 Not Found" + CRLF;
+        contentTypeLine = "Content-Type: text/html" + CRLF;
         entityBody = "<HTML>" +
         "<HEAD><TITLE>Not Found</TITLE></HEAD>" +
         "<BODY>Not Found</BODY></HTML>";
 
         }   
-        // Send the status line.
-br
         
-        
-        
-        
-        
-        
-        
-        
-        */
+        os.writeBytes(statusLine);
+       // Send the content type line.
+        os.writeBytes(contentTypeLine);
+        // Send a blank line to indicate the end of the header lines.
+        os.writeBytes(CRLF);
 
+
+        if (fileExists) {
+        sendBytes(fis, os);
+        fis.close();
+        } else {
+        os.writeBytes(entityBody);
+        }        
         // Close streams and socket.
         os.close();
         br.close();
         socket.close();
+    }
+
+    private static void sendBytes(FileInputStream fis, OutputStream os) throws Exception
+    {
+        // Construct a 1K buffer to hold bytes on their way to the socket.
+        byte[] buffer = new byte[1024];
+        int bytes = 0;
+        // Copy requested file into the socket's output stream.
+        //System.out.println(bytes + "+" + fis.read(buffer));
+        while((bytes = fis.read(buffer)) != -1 ) {
+        os.write(buffer,0, bytes);
+        }
+    }
+
+    private static String contentType(String fileName)
+    {
+        if(fileName.endsWith(".htm") || fileName.endsWith(".html")) {
+        return "text/html";
+        }
+        if(fileName.endsWith(".gif")) {
+        return "image/gif";
+        }
+        if(fileName.endsWith(".jpg") || fileName.endsWith(".jpeg")) {
+        return "image/jpeg";
+        }
+        return "application/octet-stream";
     }
 }
 
